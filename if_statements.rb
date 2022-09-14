0d983d41437cc1cc370ae894154a14249fe01c8c
Alex Pitzel <pitzelalex@gmail.com>
2022-09-14 14:07:15 -0400

    Add if_statements pizza

--- if_statements.rb
+++ if_statements.rb
@@ -63,5 +63,11 @@ end
 # Experiment with manipulating the value held within both variables
 # to make sure all above conditions output what you expect.
 
-cups_of_flour = 1
+cups_of_flour = 2
 has_sauce = true
+
+if cups_of_flour >= 2 and has_sauce == true
+  puts "I can make pizza"
+elsif cups_of_flour < 2 or has_sauce != true
+  puts "I cannot make pizza"
+end
