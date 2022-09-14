9768f7fae776cf5c7b69db6b6456dee641c288ad
Alex Pitzel <pitzelalex@gmail.com>
2022-09-14 15:11:45 -0400

    Add code challenge medium

---
+++ code_challenges/code_challenge_medium.rb
@@ -0,0 +1,14 @@
+# Code Challenge Medium
+
+#Variable
+num = 7
+
+#Code
+if (num%3) == 0 and (num%5) == 0
+  puts "FizzBuzz"
+elsif (num%3) == 0
+  puts "Fizz"
+elsif (num%5) == 0
+  puts "Buzz"
+elsif puts num
+end
